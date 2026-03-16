5581465973ccf8066de54c59d770d5e18bed58ca
Terry Biesboer <tebiesboer@student.fullsail.edu>
2026-03-15 21:16:26 -0500

    Fix: Final attempt at Express 5 wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/v1/pokemon', pokemonRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('/:any*', (req, res) => {
+  app.get('/:path*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
