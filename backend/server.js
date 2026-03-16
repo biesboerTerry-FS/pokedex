d2cebcdaa095ee019d996d1a3d53842a0ce2f520
Terry Biesboer <tebiesboer@student.fullsail.edu>
2026-03-15 21:30:19 -0500

    Switch to Regex for Express 5 compatibility

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/v1/pokemon', pokemonRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('/:path*', (req, res) => {
+  app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
