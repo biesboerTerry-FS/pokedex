60b82271f01535f85bacf4df14867c71e06bcd8e
Terry Biesboer <tebiesboer@student.fullsail.edu>
2026-03-23 21:03:25 -0500

    feat: update app icon and splash screen

--- mobile/android/app/src/main/java/tsunRise/pkg/MainActivity.kt
+++ mobile/android/app/src/main/java/tsunRise/pkg/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pokedexmobile
+package tsunRise.pkg
 
 import android.os.Build
 import android.os.Bundle
--- mobile/android/app/src/main/java/tsunRise/pkg/MainApplication.kt
+++ mobile/android/app/src/main/java/tsunRise/pkg/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pokedexmobile
+package tsunRise.pkg
 
 import android.app.Application
 import android.content.res.Configuration
